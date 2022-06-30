668c4c3c571034cff6debb680fda1a4805ebb491
Rino Satya Putra <rinosatyaputra.id@gmail.com>
2022-06-30 09:44:39 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Home from "./pages/Home";
 function App() {
 	return (
-		<Router>
-			<Route exact path="/auth/login" component={Login} />
-
-			<Route exact path="/auth/register" component={Register} />
-			<Route exact path="/home" component={Home} />
-			<Route exact path="/profile" />
-			<Route exact path="/skill" />
-			<Route exact path="/master-skill" />
-		</Router>
+		<h1>Sample Boilerplate React App</h1>
 	);
 }
 
